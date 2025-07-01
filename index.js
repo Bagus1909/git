9e43b43c81bc787e5dd80a88d291eeb17abe0146
Bagus1909 <bagussetiawan19092000@gmail.com>
2025-07-01 08:25:52 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var sum = function (a, b) {
+    return a + b;
+};
+var subtract = function (a, b) {
+    return a - b;
+};
+var multiply = function (a, b) {
+    return a * b;
+};
+console.log("Sum:", sum(2, 3));
+console.log("Subtract:", subtract(5, 3));
+console.log("Multiply:", multiply(4, 2));
---
+++ index.ts
@@ -0,0 +1,16 @@
+const sum = (a: number, b: number): number => {
+    return a + b
+}
+
+const subtract = (a: number, b: number): number => {
+    return a - b
+}
+
+const multiply = (a: number, b: number): number => {
+    return a * b
+}
+
+
+console.log("Sum:", sum(2, 3));
+console.log("Subtract:", subtract(5, 3));
+console.log("Multiply:", multiply(4, 2));
